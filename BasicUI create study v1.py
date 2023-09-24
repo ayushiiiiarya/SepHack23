dfec52e9fb40b5526ce2d81e9f99f7e4c1ea8921
ayushiiiiarya <100833055+ayushiiiiarya@users.noreply.github.com>
2023-09-24 15:48:19 +0530

    Add files via upload

--- BasicUI create study v1.py
+++ BasicUI create study v1.py
@@ -11,24 +11,35 @@ from textual.containers import Vertical, Horizontal, Center
 from textual import on
 from textual_fspicker import FileOpen, FileSave, Filters, SelectDirectory
 
-import sys,os
+ 
+
+import sys,os, webbrowser
 import subprocess
 import time
+
+chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+
 
 TEXT = """\
 Main UI 
 """
 class HeaderApp(App):
 
+ 
+
     BINDINGS = [
         Binding(key="q", action="quit", description="Quit the app"),
     ]
 
+ 
+
     def on_mount(self) -> None:
         self.title = "CLI TUI"
         self.sub_title = " PLEXOS Cloud"
-        
+
         CSS_PATH = "css.tcss"
+
+ 
 
     def compose(self) -> ComposeResult:
         yield Header(id="header")  # for header
@@ -46,22 +57,30 @@ class HeaderApp(App):
                 list_mount="#main_container"
             ),
 
+ 
+
             id="main_container"
         )
+        with Horizontal():
+            yield Button("Create Study", id="open")
+            yield Label("Press the button to pick something")
+
+ 
 
         yield Horizontal(
             VerticalScroll(
                 # Button.error("Options:", disabled=True),
-                Button("Create Study", classes="sidebar"),
+                #Button("Create Study", classes="sidebar"),
                 Button("Versioning", classes="sidebar"),
                 Button("Run", classes="sidebar"),
             ),
             id="sidebar",  # for sidebar
         )
         yield Static(TEXT, id="body")  # for docking the sidebar
-        with Horizontal():
-            yield Button("Open a file", id="open")
-            yield Label("Press the button to pick something")
+
+ 
+
+ 
 
     def on_select_changed(self, event: Select.Changed) -> None:
         if str(event.value) == "0":
@@ -77,29 +96,40 @@ class HeaderApp(App):
            time.sleep(5)
            result = subprocess.check_output("pxc auth login", shell=True, stderr=subprocess.STDOUT, text=True)
 
+ 
+
     def on_button_pressed(self, event: Button.Pressed) -> None:
-        if str(event.button._reactive_label) == "Create Study":
-            result = subprocess.check_output("pxc auth login", shell=True, stderr=subprocess.STDOUT, text=True)
-        elif str(event.button._reactive_label) == "Versioning":
-           result = subprocess.check_output("pxc environment set NA", shell=True, stderr=subprocess.STDOUT, text=True)
+        #if str(event.button._reactive_label) == "Create Study":
+            #result = subprocess.check_output("pxc auth login", shell=True, stderr=subprocess.STDOUT, text=True)
+        if str(event.button._reactive_label) == "Versioning":
+           
+           file1 = open("tui_demo.txt","r")
+           path2= file1.read()
+           study_id = os.listdir(path2+"\\.plexoscloud")[0]
+           ver_cmd = 'pxc study changeset push --studyId "' +study_id+'" --message "Chaged using TUI"'
+           result = subprocess.check_output(ver_cmd, shell=True, stderr=subprocess.STDOUT, text=True)
+           
         elif str(event.button._reactive_label) == "Run":
-           result = subprocess.check_output("pxc environment set NA", shell=True, stderr=subprocess.STDOUT, text=True)
+           file1 = open("tui_demo.txt","r")
+           path2= file1.read()
+           study_id = os.listdir(path2+"\\.plexoscloud")[0]
+           cloud_url = "https://cloud-web-eeprod-na.energyexemplar.com/studies/" + study_id + "/models"
+           #result = subprocess.check_output(webbrowser.get(chrome_path).open(cloud_url), shell=False, stderr=subprocess.STDOUT, text=False)
+           result = subprocess.run(webbrowser.open_new_tab(cloud_url))
 
     def show_selected(self, to_show: Path | None) -> None:
-        #if to_show is not None:
-        #    label_text = str(to_show)
-        #else:
-        #   label_text = ""
-
-        #self.query_one(Label).update(label_text)
-
-        #print(str(to_show))
         path = str(to_show)
+        folder_path = path.split("\\")
+        folder_path = folder_path[0:-1]
+        listToStr = '\\'.join(map(str, folder_path))
+        f = open("tui_demo.txt", "w")
+        f.write(listToStr)
+        f.close()
         xml_base_name = os.path.basename(to_show)
         xml_name=xml_base_name[0:-4]
         cmd = 'pxc study create --name "' +  xml_name + '" --database "' + path + '" --description "Created by TUI"'
         result = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT, text=True)
-        
+
     @on(Button.Pressed, "#open")
     def open_file(self) -> None:
         """Show the `FileOpen` dialog when the button is pushed."""
@@ -113,6 +143,10 @@ class HeaderApp(App):
             callback=self.show_selected,
         )
 
+
+ 
+
+ 
 
 if __name__ == "__main__":
     app = HeaderApp()
